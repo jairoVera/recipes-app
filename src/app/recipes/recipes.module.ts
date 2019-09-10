336e600b6c9c17420f6e5bbcc7121f1c1c4059ba
Vera <jairo.vera@us.sogeti.com>
2019-09-10 14:13:55 -0500

    Broke AppModule and AppRoutingModule into seperate feature Modules for Recipes and ShoppingList

---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
+
+import { RecipesComponent } from './recipes.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipesRoutingModule } from './recipes-routing.module';
+
+@NgModule({
+    declarations: [
+        RecipesComponent,
+        RecipeStartComponent,
+        RecipeListComponent,
+        RecipeDetailComponent,
+        RecipeItemComponent,
+        RecipeEditComponent
+    ],
+    imports: [
+        ReactiveFormsModule,
+        RecipesRoutingModule,
+        SharedModule
+    ]
+})
+export class RecipesModule {}
