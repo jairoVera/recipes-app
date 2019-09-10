344a5aaa5a8b917fc42ac8d24ba64b4c4568aa24
Vera <jairo.vera@us.sogeti.com>
2019-09-10 11:48:09 -0500

    Dynamically add alert modal when login credentials are wrong

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,8 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { AlertComponent } from './shared/alert/alert.component';
+import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 
 @NgModule({
   declarations: [
@@ -33,7 +35,9 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
     DropdownDirective,
     RecipeEditComponent,
     AuthComponent,
-    LoadingSpinnerComponent
+    LoadingSpinnerComponent,
+    AlertComponent,
+    PlaceholderDirective
   ],
   imports: [
     BrowserModule,
@@ -47,6 +51,9 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
     useClass: AuthInterceptorService,
     multi: true
   }],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+    AlertComponent
+  ]
 })
 export class AppModule {}
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,22 +1,32 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ComponentFactoryResolver, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService, AuthResponseData } from './auth.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Router } from '@angular/router';
+
+import { AlertComponent } from '../shared/alert/alert.component';
+import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent implements OnInit, OnDestroy {
 
   @ViewChild('authForm', { static: false }) form: NgForm;
+  @ViewChild(PlaceholderDirective, { static: false }) alertHost: PlaceholderDirective;
+
   isLoginMode = true;
   isLoading = false;
   error = null;
 
-  constructor(private authService: AuthService, private router: Router) { }
+  private alertCloseSub: Subscription;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private compFactoryResolver: ComponentFactoryResolver) { }
 
   ngOnInit() {}
 
@@ -47,10 +57,41 @@ export class AuthComponent implements OnInit {
         this.router.navigate(['/recipes']);
       }, errorMessage => {
         this.error = errorMessage;
+        this.showErrorAlert(errorMessage);
         this.isLoading = false;
       }
     );
 
     this.form.reset();
+  }
+
+  onCloseAlert() {
+    this.error = null;
+  }
+
+  ngOnDestroy() {
+    if (this.alertCloseSub) {
+      this.alertCloseSub.unsubscribe(); 
+    }
+  }
+
+  private showErrorAlert(message: string) {
+    // Create a Factory
+    const alertCompFactory = this.compFactoryResolver.resolveComponentFactory(AlertComponent);
+
+    // Get the viewContainerRef for the host ng-template
+    const hostViewContainerRef = this.alertHost.viewContainerRef;
+    hostViewContainerRef.clear(); // clear any previous added components, etc..
+
+    // Pass component factory to .createComponent method to
+    // create and attach the component to the ng-template w/ PlaceholderDirective
+    const compRef = hostViewContainerRef.createComponent(alertCompFactory);
+
+    compRef.instance.message = message;
+
+    this.alertCloseSub = compRef.instance.close.subscribe(() => {
+      this.alertCloseSub.unsubscribe();
+      hostViewContainerRef.clear();
+    });
   }
 }
