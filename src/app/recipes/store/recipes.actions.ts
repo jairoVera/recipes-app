7e5b42da3babea204bfd4e839b70973f3c9250f5
Vera <jairo.vera@us.sogeti.com>
2019-11-05 14:04:48 -0600

    Added add, update, delete Recipe actions

--- src/app/recipes/store/recipes.actions.ts
+++ src/app/recipes/store/recipes.actions.ts
@@ -3,6 +3,9 @@ import { Recipe } from '../recipe.model';
 
 export const FETCH_RECIPES = '[Recipes] Fetch Recipes';
 export const SET_RECIPES = '[Recipes] Set Recipes';
+export const ADD_RECIPE = '[Recipes] Add Recipe';
+export const UPDATE_RECIPE = '[Recipes] Update Recipe';
+export const DELETE_RECIPE = '[Recipes] Delete Recipe';
 
 export class FetchRecipes implements Action {
     readonly type = FETCH_RECIPES;
@@ -14,6 +17,27 @@ export class SetRecipes implements Action {
     constructor(public payload: Recipe[]) {}
 }
 
+export class AddRecipe implements Action {
+    readonly type = ADD_RECIPE;
+
+    constructor(public payload: Recipe) {}
+}
+
+export class UpdateRecipe implements Action {
+    readonly type = UPDATE_RECIPE;
+
+    constructor(public payload: {index: number, recipe: Recipe}) {}
+}
+
+export class DeleteRecipe implements Action {
+    readonly type = DELETE_RECIPE;
+
+    constructor(public payload: Recipe) {}
+}
+
 export type RecipeActions =
     FetchRecipes |
-    SetRecipes;
+    SetRecipes |
+    AddRecipe |
+    UpdateRecipe |
+    DeleteRecipe;
